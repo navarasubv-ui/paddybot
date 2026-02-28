6b362cd831e2c148135c9edef31567daadc33e6f
navarasubv-ui <navarasubv@gmail.com>
2026-02-28 19:26:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -127,4 +127,5 @@ def send_whatsapp_message(to, text):
     requests.post(url, headers=headers, json=payload)
 
 if __name__ == "__main__":
-    app.run(port=5000)
+
+    app.run(host="0.0.0.0", port=5000)
